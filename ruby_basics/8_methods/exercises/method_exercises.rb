3eb864a9b12769cf07af748f52d26539bef20d22
Chris <christopher@christophervigliotti.com>
2023-04-05 16:01:56 -0400

    done!

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -6,33 +6,41 @@
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
-def ascii_translator(an_integer)
-    return an_integer.chr
+def ascii_translator(a_var)
+    a_var.chr
 end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
-
+def common_sports(current_sports, favorite_sports)
+    return current_sports.intersection(favorite_sports)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
-
+def alphabetical_list(an_array)
+    return an_array.uniq.sort
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
-# return value: a string "Today's lucky number is <number>"
-
+# return value: a string "Today's lucky number is <numÍSber>"
+def lucky_number(an_integer = '7')
+    "Today's lucky number is " + an_integer.to_s
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
-
+def ascii_code(a_character)
+    a_character.length == 1 ? a_character.ord : 'Input Error'
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -41,10 +49,22 @@ end
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
+def pet_pun(animal_type)
+    response = "I think #{animal_type}s have pet-tential!"
+    case animal_type
+    when "cat"
+        response = 'Cats are purr-fect!'
+    when "dog"
+        response = 'Dogs are paw-some!'
+    end
+    puts response
+end
 
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
-
+def twenty_first_century?(the_year)
+    the_year.between?(2001,2100)
+end
